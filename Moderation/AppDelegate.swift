ab91868044c77e7e134e31c4d482848dc6193f92
Glen Brixey <glenb@zillow.com>
2020-01-24 13:01:29 -0500

    Move background refresh code to SceneDelegate

--- Moderation/AppDelegate.swift
+++ Moderation/AppDelegate.swift
@@ -5,24 +5,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        dateEnteredBackground = Date()
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Refresh if today's date has changed since entering background.
-        if let dateEnteredBackground = dateEnteredBackground, dateEnteredBackground.daysFromNow < 0 {
-            DataManager.shared.refresh()
-        }
-        dateEnteredBackground = nil
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        dateEnteredBackground = nil
     }
 
     // MARK: UISceneSession Lifecycle
@@ -30,19 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-    // MARK: - Private
-
-    /// Date value that is stored when the app enters background.
-    private var dateEnteredBackground: Date?
 }
--- Moderation/SceneDelegate.swift
+++ Moderation/SceneDelegate.swift
@@ -3,19 +3,14 @@ import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    // MARK: - UIWindowSceneDelegate
+
     var window: UIWindow?
 
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Create the SwiftUI view that provides the window contents.
         let mainView = MainView()
-
-        // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: mainView)
@@ -24,31 +19,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        // If the scene entered background more than one day ago,
+        // we need to refresh the data manager to shift the calendar forward to the present day.
+        if let date = dateSceneEnteredBackground, date.daysFromNow < 0 {
+            DataManager.shared.refresh()
+        }
+        dateSceneEnteredBackground = nil
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        dateSceneEnteredBackground = Date()
     }
+
+    // MARK: - Private
+
+    /// Date value that is stored when the scene enters background.
+    private var dateSceneEnteredBackground: Date?
 }
